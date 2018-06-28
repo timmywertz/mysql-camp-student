f56744793d83c8768ec50e67ff0498cc0531e479
Timmy Wertz <timmylwertz@gmail.com>
2018-06-28 10:50:00 -0400

    Some Query Statements

--- student-exercises/wertz-select.sql
+++ student-exercises/wertz-select.sql
@@ -1,3 +1,7 @@
 USE rockStarDay2; 
 SHOW COLUMNS FROM band;
 SELECT ID, name, genre FROM band;
+SELECT * FROM individual WHERE lastName = 'Jennings';
+SELECT ID, LASTNAME FROM individual WHERE DeceasedDate IS NOT NULL; 
+SELECT ID, LASTNAME, BirthDate FROM individual WHERE Year(BirthDate) > 1940;
+SELECT * FROM individual WHERE ID IN (1,3,5,7,19);
