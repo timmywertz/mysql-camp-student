0fe2a149900c9560f98d620106d3b00a5aaec79b
Timmy Wertz <timmylwertz@gmail.com>
2018-06-28 11:03:31 -0400

    my own query statements

--- student-exercises/wertz-select.sql
+++ student-exercises/wertz-select.sql
@@ -5,3 +5,6 @@ SELECT * FROM individual WHERE lastName = 'Jennings';
 SELECT ID, LASTNAME FROM individual WHERE DeceasedDate IS NOT NULL; 
 SELECT ID, LASTNAME, BirthDate FROM individual WHERE Year(BirthDate) > 1940;
 SELECT * FROM individual WHERE ID IN (1,3,5,7,19);
+
+SELECT name FROM band WHERE genre = 'alternative' AND isTogether = 0;
+SELECT ID, name FROM band WHERE ID IN (4);
