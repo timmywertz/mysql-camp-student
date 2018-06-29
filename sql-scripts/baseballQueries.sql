5161be1d4e961a9da7859e3325252adddbdb07b4
Timmy Wertz <timmylwertz@gmail.com>
2018-06-29 10:22:06 -0400

    More sequels and ORDER BY

--- sql-scripts/baseballQueries.sql
+++ sql-scripts/baseballQueries.sql
@@ -23,4 +23,45 @@ DESCRIBE vteamRoster;
 
 SHOW CREATE VIEW vteamRoster;
 
+SELECT *
+FROM player
+LEFT JOIN batting ON player.ID = batting.playerID;
+
+SELECT *
+FROM team
+LEFT JOIN roster ON team.ID = roster.teamID;
+
+SELECT * 
+FROM team
+INNER JOIN roster ON team.ID = roster.teamID;
+
+SELECT *
+FROM team
+LEFT JOIN roster ON team.ID = roster.teamID
+WHERE roster.playerID IS NULL;
+
+SELECT batting.rank, batting.battingAvg, CONCAT(player.lastName,", ",player.firstName)
+FROM player
+INNER JOIN batting ON batting.playerID = player.ID;  
+
+SELECT batting.battingAvg, batting.id, team.teamname 
+FROM team
+INNER JOIN batting ON batting.teamID = team.ID
+ORDER BY battingAvg DESC; 
+
+SELECT * FROM baseball.vbattingleaders;
+
+SELECT firstName, lastName, ABBR, hits  
+FROM baseball.vbattingleaders 
+ORDER BY hits DESC;
+
+SELECT firstName, lastName, ABBR, hits  
+FROM baseball.vbattingleaders 
+ORDER BY hits ASC;
+
+SELECT *  
+FROM baseball.vbattingleaders 
+ORDER BY ABBR, atbats DESC;
+
+
 
